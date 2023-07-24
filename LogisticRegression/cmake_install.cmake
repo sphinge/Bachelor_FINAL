3137ff44e08957c7a3942721d125a0955b5dae69
Mak <mayank.gulati@fu-berlin.de>
2023-07-24 14:42:12 +0200

    working LR

--- LogisticRegression/cmake_install.cmake
+++ LogisticRegression/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/wiktoria/Desktop/Thesis/LogisticRegression
+# Install script for directory: /home/mayag88/Documents/Code/new_bachelor/LogisticRegression
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/wiktoria/Desktop/Thesis/LogisticRegression/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/mayag88/Documents/Code/new_bachelor/LogisticRegression/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
