4c17d7dd6076235123e6d86eaaf8a187766c5d3c
WIktoria Krawczyk <139447338+sphinge@users.noreply.github.com>
2023-09-08 12:03:53 +0200

    Delete RIOT_Network directory

--- RIOT_Network/client/bin/native/gnrc_rpl/gnrc_rpl_auto_init.d
+++
@@ -1,6 +0,0 @@
-/home/wiktoria/Desktop/Thesis/RIOT_Network/client/bin/native/gnrc_rpl/gnrc_rpl_auto_init.o: \
- /home/wiktoria/Desktop/Thesis/RIOT/sys/net/gnrc/routing/rpl/gnrc_rpl_auto_init.c \
- /usr/include/stdc-predef.h \
- /home/wiktoria/Desktop/Thesis/RIOT_Network/client/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/home/wiktoria/Desktop/Thesis/RIOT_Network/client/bin/native/riotbuild/riotbuild.h:
--- RIOT_Network/server/bin/native/cpu/tramp.d
+++
@@ -1,6 +0,0 @@
-/home/wiktoria/Desktop/Thesis/RIOT_Network/server/bin/native/cpu/tramp.o: \
- /home/wiktoria/Desktop/Thesis/RIOT/cpu/native/tramp.S \
- /usr/include/stdc-predef.h \
- /home/wiktoria/Desktop/Thesis/RIOT_Network/server/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/home/wiktoria/Desktop/Thesis/RIOT_Network/server/bin/native/riotbuild/riotbuild.h:
